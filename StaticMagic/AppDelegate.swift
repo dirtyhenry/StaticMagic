4d83ba963f00bd3392cd9152a7e4a48d1e697c27
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2020-10-01 20:47:52 +0200

    Initial Commit

---
+++ StaticMagic/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  StaticMagic
+//
+//  Created by Mickaël Floc'hlay on 01/10/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StaticMagic/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StaticMagic
+//
+//  Created by Mickaël Floc'hlay on 01/10/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
